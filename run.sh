7bce8cbd9b8ea04a29eaef86305fae115d1b94d2
Miles Gould <miles@assyrian.org.uk>
2013-06-07 11:13:09 +0100

    Create DB if it doesn't yet exist.

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+if [ ! -e gds.sqlite3 ]; then
+        sqlite3 gds.sqlite3 < schema.sql
+fi
 ARGS=`perl -E 'say $_ for 0..10000'`
 ghc -rtsopts managetestgroup.hs &&
         time ./managetestgroup +RTS -h -RTS creategroup -g barney $ARGS
