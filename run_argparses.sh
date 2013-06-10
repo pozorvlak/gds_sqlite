7a98bb633b7f13662602ca307e5230bdad632cf8
Miles Gould <miles@assyrian.org.uk>
2013-06-10 12:02:19 +0100

    Profile argparse_cmdargs.

--- run_argparses.sh
+++ run_argparses.sh
@@ -2,8 +2,8 @@
 
 ARGS=$(seq 1 10000)
 ghc argparse_getargs.hs || exit 2
-ghc argparse_cmdargs.hs || exit 3
+ghc -rtsopts -prof -fprof-auto argparse_cmdargs.hs || exit 3
 echo "Testing System.Environment.getArgs with 10,000 arguments..."
 time ./argparse_getargs $ARGS
 echo "Testing System.Console.CmdArgs.cmdArgs with 10,000 arguments..."
-time ./argparse_cmdargs $ARGS
+time ./argparse_cmdargs +RTS -p -RTS $ARGS
