971badfe0fe6e981151bdae0321ad0ab29140dd6
Miles Gould <miles@assyrian.org.uk>
2013-06-11 16:09:20 +0100

    Remove everything except minimal testcases.

--- ResultsDB.hs
+++
@@ -1,7 +0,0 @@
-module ResultsDB (getConnectionFromTrunk) where
-
-import Database.HDBC.Sqlite3(Connection, connectSqlite3)
-
-getConnectionFromTrunk :: IO Connection
-getConnectionFromTrunk = connectSqlite3 "gds.sqlite3"
-
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -e gds.sqlite3 ]; then
-        sqlite3 gds.sqlite3 < schema.sql
-fi
-ARGS=$(seq 1 10000)
-ghc -rtsopts managetestgroup.hs &&
-        time ./managetestgroup +RTS -h -RTS creategroup -g barney $ARGS
